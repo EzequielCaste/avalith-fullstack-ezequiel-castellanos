6d0365e30445942f5daee928c42f9e8b2c37c7a9
EzequielCaste <ezequiel.caste@gmail.com>
2021-08-25 22:00:42 -0300

    create endpoint for user login

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,7 +1,9 @@
 const {validationResult} = require('express-validator');
 const bcrypt = require('bcrypt');
-const {connect} = require('../config/db');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
+const {connect} = require('../config/db');
 
 const register = async (req, res) => {
   const client = connect();
@@ -29,6 +31,7 @@ const register = async (req, res) => {
 
   client.query(query, values)
     .then(resp => {
+      client.end();
       return res.status(200).json({
         ok: true,
         msg: 'User created',
@@ -36,15 +39,71 @@ const register = async (req, res) => {
       });
     })
     .catch(err => {
+      client.end();
       return res.status(500).json({
         ok: false,
         msg: 'Error when registering user.',
         err,
       });
     });
-  await client.end();
+  client.end();
+};
+
+const login = async (req, res) => {
+  const client = connect();
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.array(),
+    });
+  }
+
+  const {email, password} = req.body;
+
+  const query = `
+  select email, password from users
+  where email = $1
+  `;
+
+  client.query(query, [email])
+    .then(resp => {
+      if (resp.rowCount === 0) {
+        return res.status(404).json({
+          ok: false,
+          msg: 'User not found',
+        });
+      } else {
+        if (bcrypt.compareSync(password, resp.rows[0].password)) {
+          // user password ok send token
+          const payload = {
+            email,
+          };
+          const token = jwt.sign(payload, process.env.JWT_SECRET);
+          return res.status(200).json({
+            ok: true,
+            msg: 'Login sucessful',
+            token,
+          });
+        } else {
+          return res.status(400).json({
+            ok: false,
+            msg: 'Authentication failed',
+          });
+        }
+      }
+    })
+    .catch(err => {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Database error',
+      });
+    });
+  client.end();
 };
 
 module.exports = {
   register,
+  login,
 };
--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,18 @@ const router = express.Router();
 const {check} = require('express-validator');
 const userController = require('../controllers/users.controller');
 
+// /users Routes
+
 // register user
-router.post('/', [
+router.post('/register', [
   check('email').not().isEmpty().isEmail().withMessage('Email is not valid'),
   check('password').isLength({min: 3}).withMessage('Password must be at least 3 characters'),
   userController.register,
 ]);
 
+router.post('/login', [
+  check('email').not().isEmpty().isEmail().withMessage('Email is not valid'),
+  check('password').isLength({min: 3}).withMessage('Password must be at least 3 characters'),
+  userController.login,
+]);
 module.exports = router;
